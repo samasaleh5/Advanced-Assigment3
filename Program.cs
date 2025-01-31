7bd12322fe6673fcb91c5ed4bd7964085955f172
samasaleh5 <ss2104@fayoum.edu.eg>
2025-01-31 13:13:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced_Assigment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
